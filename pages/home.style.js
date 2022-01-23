eb99bf8a4db3a294379a87647e5726559b299081
Monkey <florentheyraud.pro@gmail.com>
2022-01-23 14:57:16 +0100

    add presentation section in home page

--- pages/home.style.js
+++ pages/home.style.js
@@ -3,3 +3,21 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   text-align: center;
 `;
+
+export const PagesSection = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 5px;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 50px;
+`;
+
+export const PageSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: whitesmoke;
+  width: 100%;
+  min-height: 200px;
+  border-radius: 10px;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
 import styled from "styled-components";
 import Meta from "@root/components/core/Meta";
 
-import { Inner } from "@root/styles/Global";
-import { Wrapper } from "./home.style";
+import { A, Inner } from "@root/styles/Global";
+import { Wrapper, PagesSection, PageSection } from "./home.style";
+import Link from "next/link";
+import { prettyName } from "@root/utils/upperCase";
 
 const meta = {
   name: "Microbiome studio - Accueil",
   description:
     "Accueil du site web de Microbiome studio pour la gestion de projets ",
 };
+
+const pagesSections = [
+  { title: "organisations", path: "/organisations" },
+  { title: "membres", path: "/membres" },
+  { title: "projets", path: "/projets" },
+];
+
+const pageSectionsDisplay = pagesSections.map((pageSection) => {
+  const { title, path } = pageSection;
+
+  return (
+    <PageSection>
+      <h3>{prettyName(title)}</h3>
+      <Link href={path} passHref>
+        <A>Accéder à la liste des {title}</A>
+      </Link>
+    </PageSection>
+  );
+});
 
 export default function Home() {
   return (
@@ -17,9 +38,7 @@ export default function Home() {
       <Wrapper>
         <h1>MiCROBIOME STUDIO</h1>
         <h2>APPLICATION DE GESTION DE PROJETS</h2>
-        <ul>
-          <li></li>
-        </ul>
+        <PagesSection>{pageSectionsDisplay}</PagesSection>
       </Wrapper>
     </Inner>
   );
